0e9cb51b20863f81fc9a0dc6544ad48ea82fb4c8
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-10-14 12:53:15 +0200

    feat: implement ls user command handling

--- src/commands/handleLsUserCommand.js
+++ src/commands/handleLsUserCommand.js
@@ -1,3 +1,35 @@
-export const handleLsUserCommand = (directory) => {
-  return directory;
+import { readdir } from "fs/promises";
+import { OPERATION_FAILED_MESSAGE } from "../constants/constants.js";
+
+export const handleLsUserCommand = async (currentDirectory) => {
+  try {
+    const readedItems = await readdir(currentDirectory, {
+      withFileTypes: true,
+    });
+
+    const { files, folders } = readedItems.reduce(
+      (acc, currItem) => {
+        if (currItem.isFile()) {
+          acc.files.push({ name: currItem.name, type: "File" });
+        } else if (currItem.isDirectory()) {
+          acc.folders.push({ name: currItem.name, type: "Folder" });
+        }
+
+        return acc;
+      },
+      {
+        files: [],
+        folders: [],
+      },
+    );
+
+    folders.sort((a, b) => a.name.localeCompare(b.name));
+    files.sort((a, b) => a.name.localeCompare(b.name));
+
+    console.table([...folders, ...files], ["name", "type"]);
+  } catch (error) {
+    console.error(OPERATION_FAILED_MESSAGE, error);
+  }
+
+  return currentDirectory;
 };
